c6e257f33a1e19d63761126649838dc52d37225a
adhensatriani <54734116+adhensatriani@users.noreply.github.com>
2019-10-08 11:17:43 +0700

    Add files via upload

---
+++ test161.js
@@ -0,0 +1,45 @@
+<style>
+/* iframe post */
+.div-frame {
+  position: relative;
+  width: 100%;
+  height: 445px;
+  overflow: hidden;
+  font-family:-apple-system,BlinkMacSystemFont,"Roboto","Segoe UI","Oxygen-Sans","Ubuntu","Cantarell","Helvetica Neue",sans-serif;
+  background:black;
+}
+.div-frame .frame {
+  position: relative;
+  padding-bottom: 56.25%;
+  height: 0;
+  overflow: hidden;
+  margin: 0 auto;
+  width: 100%;
+  height: 100%;
+}
+.div-frame .frame iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border: 0
+}
+.div-frame:hover ul.tab-server,
+ul.sub-server,
+ul.tab-server {
+  transition: all .4s ease-in-out
+}
+</style>
+
+<script type="text/javascript">
+<!--
+
+document.write( '<div class=\"div-frame\">\n' );
+document.write( '<div class=\"frame\">\n' );
+document.write( '<iframe allow=\"autoplay; encrypted-media\" allowfullscreen=\"\" frameborder=\"0\" id=\"iframe\" name=\"search_iframe\" scrolling=\"no\" src=\"http://tvonlineapps.blogspot.com/p/ch16.html\"></iframe>\n' );
+document.write( '  </div>\n' );
+document.write( '</div>' );
+
+//-->
+</script>
