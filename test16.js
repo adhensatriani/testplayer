c49654bed3bbbfd5c8c168de47753ce88c73f633
adhensatriani <54734116+adhensatriani@users.noreply.github.com>
2019-10-08 11:05:26 +0700

    Add files via upload

---
+++ test16.js
@@ -0,0 +1,11 @@
+<script type="text/javascript">
+<!--
+
+document.write( '<div class=\"div-frame\">\n' );
+document.write( '<div class=\"frame\">\n' );
+document.write( '<iframe allow=\"autoplay; encrypted-media\" allowfullscreen=\"\" frameborder=\"0\" id=\"iframe\" name=\"search_iframe\" scrolling=\"no\" src=\"http://tvonlineapps.blogspot.com/p/ch16.html\"></iframe>\n' );
+document.write( '  </div>\n' );
+document.write( '</div>' );
+
+//-->
+</script>
